86fd5823f5d1737df7964bae96b9f3312b6d146a
Alukard <kirill1974111@gmail.com>
2023-12-08 09:29:41 +0100

    category fix

--- src/Components/SingleCategory/CategoryBanner.jsx
+++ src/Components/SingleCategory/CategoryBanner.jsx
@@ -5,7 +5,7 @@ export default function CategoryBanner({category}){
 
     return(
         <div className=" tablet:flex-1 w-full dark:bg-secondaryDarkColor bg-LightSecondaryDarkColor px-10 rounded-lg relative pb-20 mx-auto tablet:mx-0">
-            <h1 className=" text-mainColor uppercase text-center tracking-widest font-bold desktop:text-[120px] sm-tablet:text-[55px] text-[25px] mt-10 mb-5 laptop:mb-0 tablet:mt-5 desktop:mt-0">{category?.name}</h1>
+            <h1 className=" text-mainColor uppercase text-center tracking-widest font-bold desktop:text-[107px] sm-tablet:text-[55px] text-[25px] mt-10 mb-5 laptop:mb-0 tablet:mt-5 desktop:mt-5">{category?.name}</h1>
             <div className="truncate tablet:h-[80px]  h-[60px] rounded-lg desktop:w-1/2 w-3/4 mx-auto hover:w-full transition-all hover:rounded-2xl mb-10">
                 <img src={category?.image} className="w-full mx-auto desktop:mt-[-300px] sm-tablet:mt-[-100px] mt-[-40px]" />
             </div>
